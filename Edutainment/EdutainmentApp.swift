5c3856bbe586c1cebe43165feae048f6809edbf6
Sean Dooley <s.dooley@ctidigital.com>
2024-11-11 14:34:31 +0000

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Sean Dooley on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
